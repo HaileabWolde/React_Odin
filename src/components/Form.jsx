290ed89ae5ba8525bf18cb126047376a82618e80
Haileab <womahaileab1428@gmail.com>
2025-12-03 17:02:40 +0300

    working on the form componenet

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,15 @@
 import '../styles/Form.css'
-import { FaGraduationCap, FaChevronDown  } from 'react-icons/fa';
+import { FaGraduationCap, FaChevronDown, FaSuitcase, FaTrash   } from 'react-icons/fa';
 
 function Form () {
     return (
         <div className="Form">
             <div className='clearResume'>
-                    
+                    <span>
+                    <FaTrash color='red' fontSize={20}/>
+                    Clear Resume
+                    </span>
+                    <p>Load an Example</p>
             </div>
             <form className='personalDetails'>
                    <h1>Personal Details</h1>
@@ -32,16 +36,60 @@ function Form () {
                    
             </form>
             <form className='educationDetails'>
-                    <div className='educationNav'>
-                             <FaGraduationCap size={24} fontWeight={900} />
-                             <span>Graduation</span>
+                    <div className='sectionNav'>
+                             <span> <FaGraduationCap size={24} fontWeight={900} /> Graduation</span>
                              <FaChevronDown/>
 
                     </div>
+                     <label for="School">
+                    School
+                   </label>
+                   <input type='text' placeholder='Enter School/University'>
+                   </input>
+                   <label for="Degree">
+                    Enter Degree/Field of Study
+                   </label>
+                   <input type='text' placeholder='Enter Your Degree'>
+                   </input>
+                   <label for="startDate">
+                    Start Date
+                   </label>
+                   <input type="date" placeholder="startDate"></input>
+                    <label for="endDate">
+                    End Date
+                   </label>
+                   <input type="date" placeholder="endDate"></input>
+                  
             
             </form>
             <form className='expreienceDetails'>
-                   
+                   <div className='sectionNav'>
+                           <span> <FaSuitcase size={20} fontWeight={900} /> Expreience</span>
+                             <FaChevronDown/>
+                   </div>
+                    <label for="Company">
+                    Company Name
+                   </label>
+                   <input type='text' placeholder='Enter Company Name'>
+                   </input>
+                   <label for="Position">
+                    Enter Position 
+                   </label>
+                   <input type='text' placeholder='Enter Your Role'>
+                   </input>
+                   <label for="startDate">
+                    Start Date
+                   </label>
+                   <input type="date" placeholder="startDate"></input>
+                    <label for="endDate">
+                    End Date
+                   </label>
+                   <input type="date" placeholder="endDate"></input>
+                   <label for="Description">
+                        Description
+                   </label>
+                  < textarea  rows="5" cols="50"></textarea>
+                  
             </form>
         </div>
     )
