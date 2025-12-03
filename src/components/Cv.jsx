2ad4cb915b9a2e1cfe2a65843427ad0b86ffabdd
Haileab <womahaileab1428@gmail.com>
2025-12-03 16:17:01 +0300

    started working on form

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -2,7 +2,7 @@ import '../styles/Cv.css'
 function CV () {
     return (
         <div className="Cv">
-    
+            <h1>HELLO</h1>
      </div>
     )
     
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,48 @@
 import '../styles/Form.css'
+import { FaGraduationCap, FaChevronDown  } from 'react-icons/fa';
+
 function Form () {
     return (
         <div className="Form">
             <div className='clearResume'>
                     
             </div>
-            <div className='personalDetails'>
+            <form className='personalDetails'>
+                   <h1>Personal Details</h1>
+                   <label for="fullName">
+                    FullName
+                   </label>
+                   <input type='text' placeholder='Enter Your Full Name'>
+                   </input>
+                   <label for="Email">
+                    Email
+                   </label>
+                   <input type='email' placeholder='you@example.com'>
+                   </input>
+                   <label for="PhoneNumber">
+                    Phone Number
+                   </label>
+                   <input type='tel' placeholder='Enter Your Phone Number'>
+                   </input>
+                   <label for="Address">
+                    Address
+                   </label>
+                   <input type='text' placeholder='City, Country'>
+                   </input>
                    
-            </div>
-            <div className='educationDetails'>
-                    
-            </div>
-            <div className='expreienceDetails'>
+            </form>
+            <form className='educationDetails'>
+                    <div className='educationNav'>
+                             <FaGraduationCap size={24} fontWeight={900} />
+                             <span>Graduation</span>
+                             <FaChevronDown/>
+
+                    </div>
+            
+            </form>
+            <form className='expreienceDetails'>
                    
-            </div>
+            </form>
         </div>
     )
    
